cd4a1d85221957a77ac1e38958e3996f69050ec3
iAkash <Akashsah946@gmail.com>
2023-08-06 08:56:58 +0545

    feat: adds adj matrix to adj list conversion method

--- RepresentationOfGraph.java
+++ RepresentationOfGraph.java
@@ -13,8 +13,8 @@ public class RepresentationOfGraph {
 		 matrix[2][1]=1;
 		 
 		 //1--3
-		 matrix[1][4]=1;
-		 matrix[4][1]=1;
+		 matrix[1][3]=1;
+		 matrix[3][1]=1;
 		 
 		 //2--4
 		 matrix[2][4]=1;
@@ -32,6 +32,7 @@ public class RepresentationOfGraph {
 		 matrix[4][5]=1;
 		 matrix[5][4]=1;
 		 
+		 //traversing through the adjacency matrix
 		 for(int i=1;i<=vertices;i++) {
 			 for(int j=1;j<=edges;j++) {
 				 System.out.print(matrix[i][j]+" ");
@@ -39,10 +40,11 @@ public class RepresentationOfGraph {
 			 }
 			 System.out.println();
 		 }
+		 matrixToList(matrix, vertices, edges);
 		 
 		 //Adjacency List Representation of Graph
 		 ArrayList<ArrayList<Integer>> al = new ArrayList();
-	     for(int i=1;i<=vertices;i++) {
+	     for(int i=1;i<=vertices+1;i++) {
 	    	 al.add(new ArrayList<Integer>());
 	     }
 		 
@@ -73,12 +75,57 @@ public class RepresentationOfGraph {
 		 
 		 
 		 //Traversing through the Adjacency List
-		 for(int i=1;i<=al.size();i++) {
+		 for(int i=1;i<al.size();i++) {
 			 
 				 System.out.print(i+"-->{"+al.get(i)+"}");
 			
 			 System.out.println();
 		 }
+		 
+		 bfs(al);
+	}
+	public static void bfs(ArrayList<ArrayList<Integer>> al){
+		Queue<Integer> queue = new LinkedList<>();
+		int visited[] = new int[al.size()];
+		ArrayList<Integer> result = new ArrayList<>();
+		queue.add(1);
+//		queue.add(unvisisted adjacent nodes);
+//		result=queue.remove();
+		int index = queue.peek();
+		while(!queue.isEmpty()) {
+			
+			for(int i=0; i<al.get(index).size();i++) {
+				result.add(queue.peek());
+				if(visited[i+1]!=1) {
+					visited[i+1]=1;
+					queue.add(al.get(index).get(i));
+				}
+				
+				
+				
+			}
+			queue.remove();
+			index=queue.peek();
+		}
+		
+		for(int i=0;i<result.size();i++) {
+			System.out.println(result.get(i));
+		}
+		
+	}
+	public static void matrixToList(int[][] matrix, int row, int col) {
+		ArrayList<ArrayList<Integer>> List = new ArrayList<ArrayList<Integer>>();
+		for(int i=0; i<=row; i++) {
+			List.add(new ArrayList<Integer>());
+		}
+		for(int i=1; i<=row; i++) {
+			for(int j=1; j<=col; j++) {
+				if(matrix[i][j] != 0) {
+					List.get(i).add(j);
+					
+				}
+			}
+		}
 	}
 
 }
